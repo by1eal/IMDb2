c17ddf3a6d4ea3439f984330c4b536c174de0eb5
AbrahamVldz <44095388+AbrahamVldz@users.noreply.github.com>
2018-11-20 00:02:50 -0700

    Add files via upload

---
+++ IMDb1.4/boxoffice.php
@@ -0,0 +1,279 @@
+<? php
+<div class="article">
+<span class="ab_widget">
+<h1 class="header">Top Box Office (US)</h1>
+<h4>Weekend of november 16 - 18, 2018></h4>
+<br>
+<table class="chart full-width" data-caller-name="chart-boxoffice">
+<colgroup>
+	<col class"chartTableColumnPoster">
+	<col class"chartTableColumnTittle">
+	<col class"chartTableColumnGross">
+	<col class"chartTableColumnWeeks">
+	<col class"chartTableColumnWatchlistRibbon">
+</colgroup>
+<thead>
+	<tr>
+	<th></th>
+	<th>Tittle</th>
+	<th>Weekend</th>
+	<th>Gross</th>
+	<th>Weeks</th>
+	<th></th>
+	</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="posterColumn">
+				<a href="Fantastic Beast: The Crimes of Grindelwald"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="David Yates (dir.), Eddie Redmayne, Katherine Waterston">Fantastic Beast: The Crimes of Grindelwald</a>
+			</td>
+			<td class="ratingColumn">
+					$62.2M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$62.2M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					1
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="The Grinch"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Yarrow Cheney(dir.), Benedict Cumberbatch, Cameron Seely">The Grinch</a>
+			</td>
+			<td class="ratingColumn">
+					$38.6M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$127.0M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					2
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="Bohemian Rhapsody"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Bryan Singer (dir.), Rami Malek, Lucy Boynton">Bohemian Rhapsody</a>
+			</td>
+			<td class="ratingColumn">
+					$16.0M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$128.2M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					3
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="Instant Family"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Sean Anders (dir.), Mark Wahlberg, Rose Byrne">Instant Family</a>
+			</td>
+			<td class="ratingColumn">
+					$14.5M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$14.5M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					1
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="Widows"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Steve McQueen (dir.), Viola Davis, Michelle Rodriguez">Widows</a>
+			</td>
+			<td class="ratingColumn">
+					$12.4M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$12.4M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					1
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="The Nutcracker and the Four Realms"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Lasse Hallström (dir.), Mackenzie Foy, Keira Knightley">The Nutcracker and the Four Realms</a>
+			</td>
+			<td class="ratingColumn">
+					$4.8M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$44.0M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					3
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="A Star Is Born"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Bradley Cooper (dir.), Lady Gaga, Bradley Cooper">A Star Is Born</a>
+			</td>
+			<td class="ratingColumn">
+					$4.3M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$185.8M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					7
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="Overlord"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Julius Avery (dir.), Jovan Adepo, Wyatt Russell">Overlord</a>
+			</td>
+			<td class="ratingColumn">
+					$3.8M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$17.7M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					2
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="The Girl in the Spider's Web"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Fede Alvarez (dir.), Claire Foy, Beau Gadsdon">The Girl in the Spiders Web</a>
+			</td>
+			<td class="ratingColumn">
+					$2.5M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo">$13.3M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					2
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+		<tr>
+			<td class="posterColumn">
+				<a href="Burn the Stage: The Movie"><img src="C:\xampp\htdocs\IMDB2" width="45" height="67"></a>
+			</td>
+			<td class="tittleColumn">
+				<a href="" title="Jun-Soo Park (dir.), BTS, J-Hope">Burn the Stage: The Movie</a>
+			</td>
+			<td class="ratingColumn">
+					$2.4M
+			</td>
+			<td class="ratingColumn">
+					<span class="secondaryInfo"> $3.6M
+					</span>
+			</td>
+			<td class="weeksColumn">
+					1
+			</td>
+			<td class="watchlistColumn">
+					<div class="wlb_ribbon" data-tconst="" style="position: relative;">
+					<div class="wl-ribbon standalone not-inWL" title="Click to add to watchlist">
+					</div>
+					</div>
+			</td>
+		</tr>
+
+	 </tbody>
+	</table>
+</span>
+?>
